3ae7e297940363802016c9f869bc99576b2cb3ed
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2021-10-22 13:49:54 +0000

    leetcode 77 question

---
+++ index.js
@@ -0,0 +1,27 @@
+Given two integers n and k, return all possible combinations of k numbers out of the range [1, n].
+
+You may return the answer in any order.
+
+Example 1:
+Input: n = 4, k = 2
+Output:
+[
+  [2,4],
+  [3,4],
+  [2,3],
+  [1,2],
+  [1,3],
+  [1,4],
+]
+Constraints:
+1 <= n <= 20
+1 <= k <= n
+
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine = function(n, k) {
+    consol.log('n', n, 'K:', k)
+};
